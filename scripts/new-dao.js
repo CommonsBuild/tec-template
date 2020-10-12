bc6b2a70979aa3e99d66e70229bc8e7f0221bde3
David Llop <931684+sembrestels@users.noreply.github.com>
2020-10-12 19:59:35 +0200

    Initial test parameters for TEC

--- scripts/new-dao.js
+++ scripts/new-dao.js
@@ -1,19 +1,17 @@
 const GardensTemplate = artifacts.require("GardensTemplate")
-const MiniMeToken = artifacts.require("MiniMeToken")
-const Token = artifacts.require("Token")
 
-const DAO_ID = "gardens" + Math.random() // Note this must be unique for each deployment, change it for subsequent deployments
-const TOKEN_OWNER = "0xb4124cEB3451635DAcedd11767f004d8a28c6eE7"
+const DAO_ID = "testtec" + Math.random() // Note this must be unique for each deployment, change it for subsequent deployments
 const NETWORK_ARG = "--network"
 const DAO_ID_ARG = "--daoid"
-const NON_MINIME_COLLATERAL = "--nonminime"
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+const collateralTokenAddress = '0x0000000000000000000000000000000000000000'
+
+const HOLDERS = []
+const INITIAL_STAKES = []
 
 const argValue = (arg, defaultValue) => process.argv.includes(arg) ? process.argv[process.argv.indexOf(arg) + 1] : defaultValue
 
 const network = () => argValue(NETWORK_ARG, "local")
 const daoId = () => argValue(DAO_ID_ARG, DAO_ID)
-const nonMiniMeCollateral = () => argValue(NON_MINIME_COLLATERAL, false)
 
 const gardensTemplateAddress = () => {
   if (network() === "rinkeby") {
@@ -28,87 +26,67 @@ const gardensTemplateAddress = () => {
   }
 }
 
-const DAYS = 24 * 60 * 60
+const HOURS = 60 * 60
+const DAYS = 24 * HOURS
 const ONE_HUNDRED_PERCENT = 1e18
 const ONE_TOKEN = 1e18
 const FUNDRAISING_ONE_HUNDRED_PERCENT = 1e6
 const FUNDRAISING_ONE_TOKEN = 1e6
 
-const COLLATERAL_TOKEN_NAME = "Honey"
-const COLLATERAL_TOKEN_SYMBOL = "HNY"
-const COLLATERAL_TOKEN_DECIMALS = 18
-const COLLATERAL_TOKEN_TRANSFERS_ENABLED = true
-const COLLATERAL_BALANCE = 10e23
-
 // Create dao transaction one config
-const ORG_TOKEN_NAME = "OrgToken"
-const ORG_TOKEN_SYMBOL = "OGT"
-const SUPPORT_REQUIRED = 0.5 * ONE_HUNDRED_PERCENT
-const MIN_ACCEPTANCE_QUORUM = 0.2 * ONE_HUNDRED_PERCENT
-const VOTE_DURATION_BLOCKS = 15
-const VOTE_BUFFER_BLOCKS = 10
-const VOTE_EXECUTION_DELAY_BLOCKS = 5
+const ORG_TOKEN_NAME = "Token Engineering Commons TEST Token"
+const ORG_TOKEN_SYMBOL = "TESTTEC"
+const SUPPORT_REQUIRED = 0.6 * ONE_HUNDRED_PERCENT
+const MIN_ACCEPTANCE_QUORUM = 0.02 * ONE_HUNDRED_PERCENT
+const VOTE_DURATION_BLOCKS = 3 * DAYS / 5 // 3 days
+const VOTE_BUFFER_BLOCKS = 8 * HOURS / 5 // 8 hours
+const VOTE_EXECUTION_DELAY_BLOCKS = 24 * HOURS / 5 // 24 hours
 const VOTING_SETTINGS = [SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM, VOTE_DURATION_BLOCKS, VOTE_BUFFER_BLOCKS, VOTE_EXECUTION_DELAY_BLOCKS]
 const USE_AGENT_AS_VAULT = false
 
 // Create dao transaction two config
-const TOLLGATE_FEE = ONE_TOKEN
+const TOLLGATE_FEE = 3 * ONE_TOKEN
 const USE_CONVICTION_AS_FINANCE = true
 const FINANCE_PERIOD = 0 // Irrelevant if using conviction as finance
 
 // Create dao transaction three config
-const PRESALE_GOAL = 1000 * ONE_TOKEN
-const PRESALE_PERIOD = 14 * DAYS
-const PRESALE_EXCHANGE_RATE = FUNDRAISING_ONE_TOKEN
-const VESTING_CLIFF_PERIOD = 90 * DAYS
-const VESTING_COMPLETE_PERIOD = 360 * DAYS
+const PRESALE_GOAL = 300 * ONE_TOKEN
+const PRESALE_PERIOD = 7 * DAYS
+const PRESALE_EXCHANGE_RATE = 0.0003 * FUNDRAISING_ONE_TOKEN
+const VESTING_CLIFF_PERIOD = 3 * DAYS
+const VESTING_COMPLETE_PERIOD = 3 * 7 * DAYS // 3 weeks
 const PRESALE_PERCENT_SUPPLY_OFFERED = FUNDRAISING_ONE_HUNDRED_PERCENT
-const PRESALE_PERCENT_FUNDING_FOR_BENEFICIARY = 0.5 * FUNDRAISING_ONE_HUNDRED_PERCENT
+const PRESALE_PERCENT_FUNDING_FOR_BENEFICIARY = 0.35 * FUNDRAISING_ONE_HUNDRED_PERCENT
 const OPEN_DATE = 0
 const BUY_FEE_PCT = 0.2 * ONE_HUNDRED_PERCENT
 const SELL_FEE_PCT = 0.2 * ONE_HUNDRED_PERCENT
 
 // Create dao transaction four config
-const VIRTUAL_SUPPLY = 2
-const VIRTUAL_BALANCE = 1
+const VIRTUAL_SUPPLY = 2 // TODO
+const VIRTUAL_BALANCE = 1 // TODO
 const RESERVE_RATIO = 0.1 * FUNDRAISING_ONE_HUNDRED_PERCENT
 
 module.exports = async (callback) => {
   try {
     const gardensTemplate = await GardensTemplate.at(gardensTemplateAddress())
-    let collateralToken
-
-    if (nonMiniMeCollateral()) {
-      collateralToken = await Token.new(TOKEN_OWNER, COLLATERAL_TOKEN_NAME, COLLATERAL_TOKEN_SYMBOL)
-    } else {
-      collateralToken = await MiniMeToken.new(
-        ZERO_ADDRESS,
-        ZERO_ADDRESS,
-        0,
-        COLLATERAL_TOKEN_NAME,
-        COLLATERAL_TOKEN_DECIMALS,
-        COLLATERAL_TOKEN_SYMBOL,
-        COLLATERAL_TOKEN_TRANSFERS_ENABLED
-      )
-      collateralToken.generateTokens(TOKEN_OWNER, COLLATERAL_BALANCE)
-    }
-    console.log(`Created ${COLLATERAL_TOKEN_SYMBOL} Token: ${collateralToken.address}`)
 
     const createDaoTxOneReceipt = await gardensTemplate.createDaoTxOne(
       ORG_TOKEN_NAME,
       ORG_TOKEN_SYMBOL,
+      HOLDERS,
+      INITIAL_STAKES,
       VOTING_SETTINGS,
       USE_AGENT_AS_VAULT
     );
     console.log(`Tx One Complete. DAO address: ${createDaoTxOneReceipt.logs.find(x => x.event === "DeployDao").args.dao} Gas used: ${createDaoTxOneReceipt.receipt.gasUsed} `)
 
     const createDaoTxTwoReceipt = await gardensTemplate.createDaoTxTwo(
-      collateralToken.address,
+      collateralTokenAddress,
       TOLLGATE_FEE,
-      [collateralToken.address],
+      [collateralTokenAddress],
       USE_CONVICTION_AS_FINANCE,
       FINANCE_PERIOD,
-      collateralToken.address
+      collateralTokenAddress
     )
     console.log(`Tx Two Complete. Gas used: ${createDaoTxTwoReceipt.receipt.gasUsed}`)
 
