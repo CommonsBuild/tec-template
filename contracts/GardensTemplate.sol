76f90e1a5800a7e0675ebdfbbb33b0453e43ae54
David Llop <931684+sembrestels@users.noreply.github.com>
2020-11-07 22:45:39 +0100

    Add xdai app ids

--- contracts/GardensTemplate.sol
+++ contracts/GardensTemplate.sol
@@ -12,8 +12,9 @@ import {IConvictionVoting as ConvictionVoting} from "./external/IConvictionVotin
 import {ITollgate as Tollgate} from "./external/ITollgate.sol";
 import {IBancorMarketMaker as MarketMaker} from "./external/IBancorMarketMaker.sol";
 import {IAragonFundraisingController as Controller} from "./external/IAragonFundraisingController.sol";
+import "./appIds/AppIdsXDai.sol";
 
-contract GardensTemplate is BaseTemplate {
+contract GardensTemplate is BaseTemplate, AppIdsXDai {
     using SafeMath64 for uint64;
 
     string constant private ERROR_MISSING_MEMBERS = "MISSING_MEMBERS";
@@ -22,39 +23,6 @@ contract GardensTemplate is BaseTemplate {
     string constant private ERROR_NO_CACHE = "NO_CACHE";
     string constant private ERROR_NO_COLLATERAL = "NO_COLLATERAL";
     string constant private ERROR_NO_TOLLGATE_TOKEN = "NO_TOLLGATE_TOKEN";
-
-    /**
-    * bytes32 private constant DANDELION_VOTING_APP_ID = apmNamehash("dandelion-voting");
-    * bytes32 private constant REDEMPTIONS_APP_ID = keccak256(abi.encodePacked(apmNamehash("open"), keccak256("redemptions")));
-    * bytes32 private constant CONVICTION_VOTING_APP_ID = keccak256(abi.encodePacked(apmNamehash("open"), keccak256("conviction-voting")));
-    * bytes32 private constant TOLLGATE_APP_ID = apmNamehash("tollgate");
-    * bytes32 private constant HOOKED_TOKEN_MANAGER_APP_ID = keccak256(abi.encodePacked(apmNamehash("open"), keccak256("hooked-token-manager")));
-    * bytes32 private constant BANCOR_FORMULA_ID = apmNamehash("bancor-formula");
-    * bytes32 private constant PRESALE_ID = apmNamehash("presale");
-    * bytes32 private constant MARKET_MAKER_ID = keccak256(abi.encodePacked(apmNamehash("open"), keccak256("bancor-market-maker")));
-    * bytes32 private constant MARKETPLACE_CONTROLLER_ID = keccak256(abi.encodePacked(apmNamehash("open"), keccak256("marketplace-controller")));
-    */
-    // Local app ID's
-    // bytes32 private constant DANDELION_VOTING_APP_ID = 0x2d7442e1c4cb7a7013aecc419f938bdfa55ad32d90002fb92ee5969e27b2bf07;
-    // bytes32 private constant REDEMPTIONS_APP_ID = 0xbf9fefd9508fe20f068aa3714e9beb8c4fc6dea33dc4c75371b96140d6350d20;
-    // bytes32 private constant CONVICTION_VOTING_APP_ID = 0x589851b3734f6578a92f33bfc26877a1166b95238be1f484deeaac6383d14c38;
-    // bytes32 private constant TOLLGATE_APP_ID = 0x7075e547e73484f0736b2160fcfb010b4f32b751fc729c25b677a0347d9b4246;
-    // bytes32 private constant HOOKED_TOKEN_MANAGER_APP_ID = 0xb2d2065b829a91588c8b9a15d99acd026f6673733603c6c60e505654eb2b472d;
-
-    // Rinkeby app ID's
-       bytes32 private constant DANDELION_VOTING_APP_ID = 0x40a80c4b4050993512df39c802adec62dafeb1f0586cc15f4d34bda9c47ba468; // gardens-dandelion-voting.open.aragonpm.eth
-       bytes32 private constant REDEMPTIONS_APP_ID = 0x743bd419d5c9061290b181b19e114f36e9cc9ddb42b4e54fc811edb22eb85e9d;
-       bytes32 private constant TOLLGATE_APP_ID = 0x0d321283289e70165ef6db7f11fc62c74a7d39dac3ee148428c4f9e3d74c6d61; // tollgate.open.aragonpm.eth
-       bytes32 private constant HOOKED_TOKEN_MANAGER_APP_ID = 0x26bb91b115bf14acbdc18d75042e165321eceeb3d10d852386576bbd0ec11519; // gardens-token-manager.open.aragonpm.eth
-
-    //    bytes32 private constant CONVICTION_VOTING_APP_ID = 0xc5e2e4d7422daecfc2dc08a9932e5b9f84cf5b2f61a81ef13e257b4803b93b2f; // gardens-conviction-voting.open.aragonpm.eth
-       bytes32 private constant CONVICTION_VOTING_APP_ID = 0x2e20d4e6af7ec3f285a8c7358e357d14483ccf38e79779c3037bc70c24141038; // conviction-voting-gardens.open.aragonpm.eth
-
-    // Local and Rinkeby app ID's (no need to change between environments)
-    bytes32 private constant BANCOR_FORMULA_ID = 0xd71dde5e4bea1928026c1779bde7ed27bd7ef3d0ce9802e4117631eb6fa4ed7d;
-    bytes32 private constant PRESALE_ID = 0x5de9bbdeaf6584c220c7b7f1922383bcd8bbcd4b48832080afd9d5ebf9a04df5;
-    bytes32 private constant MARKET_MAKER_ID = 0x082190c7d2371cc21a38fcd9c3cd724dd686e838e1f7a435f9f5fbc0cb562f0f; // bancor-market-maker.open.aragonpm.eth
-    bytes32 private constant MARKETPLACE_CONTROLLER_ID = 0x11a4d82f2764c41dac72ca406a52a7392a0800047a040944b29dccd669bad8e9; // marketplace-controller.open.aragonpm.eth
 
     bool private constant TOKEN_TRANSFERABLE = true;
     uint8 private constant TOKEN_DECIMALS = uint8(18);
