3227fedc0f8a1371ac52c2c7c9e281ce0958d7ad
David Llop <931684+sembrestels@users.noreply.github.com>
2020-11-08 03:11:56 +0100

    Document script based on previous work made by @lkngtn

--- scripts/new-dao.js
+++ scripts/new-dao.js
@@ -34,66 +34,94 @@ const gardensTemplateAddress = () => {
   }
 }
 
+// Helpers, no need to change
 const HOURS = 60 * 60
 const DAYS = 24 * HOURS
+const WEEKS = 7 * DAYS
 const ONE_HUNDRED_PERCENT = 1e18
 const ONE_TOKEN = 1e18
 const FUNDRAISING_ONE_HUNDRED_PERCENT = 1e6
 const FUNDRAISING_ONE_TOKEN = 1e18
+const HOLDERS_PER_TRANSACTION = 10
 
+const BLOCKTIME = network() === "rinkeby" ? 15 : network() === "mainnet" ? 13 : 5 // 15 rinkeby, 13 mainnet, 5 xdai
+console.log(`Every ${BLOCKTIME}s a new block is mined in ${network()}.`)
+
+// CONFIGURATION
+
+// Collateral Token is used to pay contributors and held in the bonding curve reserve
 const COLLATERAL_TOKEN_NAME = "Test DAI"
 const COLLATERAL_TOKEN_SYMBOL = "tDAI"
 const COLLATERAL_TOKEN_DECIMALS = 18
 const COLLATERAL_TOKEN_TRANSFERS_ENABLED = true
 const COLLATERAL_BALANCE = 10e23
 
-// Create dao transaction one config
+// Org Token represents membership in the community and influence in proposals
 const ORG_TOKEN_NAME = "Token Engineering Commons TEST Token"
 const ORG_TOKEN_SYMBOL = "TESTTEC"
+
+// # Dandelion Voting Settings
+
+// Used for administrative or binary choice decisions with ragequit-like functionality
 const SUPPORT_REQUIRED = 0.6 * ONE_HUNDRED_PERCENT
 const MIN_ACCEPTANCE_QUORUM = 0.02 * ONE_HUNDRED_PERCENT
-const VOTE_DURATION_BLOCKS = 3 * DAYS / 5 // 3 days on xDAI, ~1 day on Rinkeby
-const VOTE_BUFFER_BLOCKS = 8 * HOURS / 5 // 8 hours on xDAI, ~2 hours & 40 minutes on Rinkeby
-const VOTE_EXECUTION_DELAY_BLOCKS = 24 * HOURS / 5 // 24 hours on xDAI, 8 hours on Rinkeby
+const VOTE_DURATION_BLOCKS = 3 * DAYS / BLOCKTIME
+const VOTE_BUFFER_BLOCKS = 8 * HOURS / BLOCKTIME
+const VOTE_EXECUTION_DELAY_BLOCKS = 24 * HOURS / BLOCKTIME
 const VOTING_SETTINGS = [SUPPORT_REQUIRED, MIN_ACCEPTANCE_QUORUM, VOTE_DURATION_BLOCKS, VOTE_BUFFER_BLOCKS, VOTE_EXECUTION_DELAY_BLOCKS]
+// Set the fee paid to the org to create an administrative vote
+const TOLLGATE_FEE = 3 * ONE_TOKEN
+// If you want to use the Agent instead of the vault allowing the community to interact with external contracts
 const USE_AGENT_AS_VAULT = false
 
-// Hatch config
-const HOLDERS_PER_TRANSACTION = 10
-const IMPACT_HOURS_CSV = path.resolve('./ih.csv'); // CSV with two columns: "hatcher address" and "impact hours"
-const IMPACT_HOURS_RATE = 1 * FUNDRAISING_ONE_TOKEN // TESTTEC / IH
 
-// Create dao transaction two config
-const TOLLGATE_FEE = 3 * ONE_TOKEN
-
-const HALFLIFE = 0.5 * DAYS
-const MAX_RATIO = 0.4 // 40 percent
-const MIN_THRESHOLD = 0.005 // 0.5 percent
+// # Conviction Voting settings
+const HALFLIFE = 0.5 * DAYS // 12 hours
+const MAX_RATIO = 0.4 // 40%
+const MIN_THRESHOLD = 0.005 // 0.5%
 const MIN_EFFECTIVE_SUPPLY = 0.0025 * ONE_HUNDRED_PERCENT // 0.25% minimum effective supply
 
-const scale = n => parseInt(n * 10 ** 7)
-const BLOCKTIME = 5 // 15 rinkeby, 13 mainnet, 5 xdai
-const CONVERTED_TIME = HALFLIFE / BLOCKTIME
-const DECAY = 1/2 ** (1 / CONVERTED_TIME) // alpha
-const WEIGHT = MAX_RATIO ** 2 * MIN_THRESHOLD // determine weight based on MAX_RATIO and MIN_THRESHOLD
-const CONVICTION_SETTINGS = [scale(DECAY), scale(MAX_RATIO), scale(WEIGHT), MIN_EFFECTIVE_SUPPLY]
 
-// Create dao transaction three config
+// # Hatch settings
+
+// How many tokens required to initialize the bonding curve
 const PRESALE_GOAL = 300 * ONE_TOKEN
+// How long should the presale period last for
 const PRESALE_PERIOD = 7 * DAYS
+// How many organization tokens per collateral token should be minted
 const PRESALE_EXCHANGE_RATE = 0.00000001 * FUNDRAISING_ONE_TOKEN
+// When is the cliff for vesting restrictions
 const VESTING_CLIFF_PERIOD = PRESALE_PERIOD + 3 * DAYS // 3 days after presale
-const VESTING_COMPLETE_PERIOD = VESTING_CLIFF_PERIOD + 3 * 7 * DAYS // 3 weeks after cliff
+// When will pre-sale contributors be fully vested
+const VESTING_COMPLETE_PERIOD = VESTING_CLIFF_PERIOD + 3 * WEEKS // 3 weeks after cliff
 const PRESALE_PERCENT_SUPPLY_OFFERED = FUNDRAISING_ONE_HUNDRED_PERCENT
+// What percentage of pre-sale contributions should go to the common pool (versus the reserve)
 const PRESALE_PERCENT_FUNDING_FOR_BENEFICIARY = 0.35 * FUNDRAISING_ONE_HUNDRED_PERCENT
+// when should the pre-sale be open, setting 0 will allow anyone to open the pre-sale anytime after deployment
 const OPEN_DATE = 0
+
+// ## Impact hours conversion config
+
+// CSV with two columns: "hatcher address" and "impact hours"
+const IMPACT_HOURS_CSV = path.resolve('./ih.csv'); 
+// Ratio received organization tokens / impact hours
+const IMPACT_HOURS_RATE = 1 * FUNDRAISING_ONE_TOKEN
+
+// # Marketplace Bonding Curve Parameterization
+
+// ## Entry and Exit fee settings
+
+// percent of each "buy" that goes to the common pool
 const BUY_FEE_PCT = 0 * ONE_HUNDRED_PERCENT
+// percent of each "sell" that goes to the common pool
 const SELL_FEE_PCT = 0.2 * ONE_HUNDRED_PERCENT
 
-// Create dao transaction four config
-const VIRTUAL_SUPPLY = 2 // TODO
-const VIRTUAL_BALANCE = 1 // TODO
-const RESERVE_RATIO = 0.1 * FUNDRAISING_ONE_HUNDRED_PERCENT
+// Bonding Curve reserve settings
+const RESERVE_RATIO = 0.1 * FUNDRAISING_ONE_HUNDRED_PERCENT // Determines reserve ratio of the bonding curve, 100 percent is a 1:1 peg with collateral asset.
+
+// Virtual Supply and Virtual balance can be used to adjust granularity of the curve, behavior will be most intuitive if you do not change these values.
+const VIRTUAL_SUPPLY = 2
+const VIRTUAL_BALANCE = 1
 
 module.exports = async (callback) => {
   try {
@@ -125,6 +153,12 @@ module.exports = async (callback) => {
     )
     console.log(`Tx One Complete. DAO address: ${createDaoTxOneReceipt.logs.find(x => x.event === "DeployDao").args.dao} Gas used: ${createDaoTxOneReceipt.receipt.gasUsed} `)
     
+    const scale = n => parseInt(n * 10 ** 7)
+    const CONVERTED_TIME = HALFLIFE / BLOCKTIME
+    const DECAY = 1/2 ** (1 / CONVERTED_TIME) // alpha
+    const WEIGHT = MAX_RATIO ** 2 * MIN_THRESHOLD // determine weight based on MAX_RATIO and MIN_THRESHOLD
+    const CONVICTION_SETTINGS = [scale(DECAY), scale(MAX_RATIO), scale(WEIGHT), MIN_EFFECTIVE_SUPPLY]
+
     const createDaoTxTwoReceipt = await gardensTemplate.createDaoTxTwo(
       collateralToken.address,
       TOLLGATE_FEE,
